ed47c7d606f3aff1058d80eb123229ebfc34c598
Gary Xue <garyx.tech@gmail.com>
2013-11-24 22:45:21 +0800

    Initial Commit

---
+++ gary1/GRY1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRY1AppDelegate.h
+//  gary1
+//
+//  Created by 薛洪 on 13-11-24.
+//  Copyright (c) 2013年 薛洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRY1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gary1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gary1
+//
+//  Created by 薛洪 on 13-11-24.
+//  Copyright (c) 2013年 薛洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRY1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRY1AppDelegate class]));
+    }
+}
